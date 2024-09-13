0b5328a0c6326f706da50cd48b500be7620cb0dd
AdnanGhannam2001 <adnanghannam21@outlook.com>
2024-09-13 13:59:58 -0400

    Fix `Dynamic Array` Size on Realloc

--- includes/array.h
+++ includes/array.h
@@ -3,12 +3,14 @@
 
 #include "common.h"
 
+typedef size_t array_size_t;
+
 #define ARRAY(name, type)                           \
     typedef struct                                  \
     {                                               \
         type *items;                                \
-        size_t count;                               \
-        size_t capacity;                            \
+        array_size_t count;                         \
+        array_size_t capacity;                      \
     } name##_t;                                     \
     void name##_init(name##_t *array);              \
     void name##_write(name##_t *array, type value); \
@@ -16,28 +18,28 @@
 
 #define ARRAY_GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
 
-#define ARRAY_IMPL(name, type)                                                                   \
-    void name##_init(name##_t *array)                                                            \
-    {                                                                                            \
-        (array)->capacity = 0;                                                                   \
-        (array)->count = 0;                                                                      \
-        (array)->items = NULL;                                                                   \
-    }                                                                                            \
-    void name##_write(name##_t *array, type value)                                               \
-    {                                                                                            \
-        if ((array)->count >= (array)->capacity)                                                 \
-        {                                                                                        \
-            (array)->capacity = ARRAY_GROW_CAPACITY((array)->capacity);                          \
-            (array)->items = (type *)realloc((array)->items, ((array)->capacity * sizeof(int))); \
-            assert(((array)->items != NULL) && "Couldn't allocate memory for array");            \
-        }                                                                                        \
-        (array)->items[(array)->count] = (value);                                                \
-        (array)->count++;                                                                        \
-    }                                                                                            \
-    void name##_free(name##_t *array)                                                            \
-    {                                                                                            \
-        free((array)->items);                                                                    \
-        name##_init(array);                                                                      \
+#define ARRAY_IMPL(name, type)                                                                            \
+    void name##_init(name##_t *array)                                                                     \
+    {                                                                                                     \
+        (array)->capacity = 0;                                                                            \
+        (array)->count = 0;                                                                               \
+        (array)->items = NULL;                                                                            \
+    }                                                                                                     \
+    void name##_write(name##_t *array, type value)                                                        \
+    {                                                                                                     \
+        if ((array)->count >= (array)->capacity)                                                          \
+        {                                                                                                 \
+            (array)->capacity = ARRAY_GROW_CAPACITY((array)->capacity);                                   \
+            (array)->items = (type *)realloc((array)->items, ((array)->capacity * sizeof(array_size_t))); \
+            assert(((array)->items != NULL) && "Couldn't allocate memory for array");                     \
+        }                                                                                                 \
+        (array)->items[(array)->count] = (value);                                                         \
+        (array)->count++;                                                                                 \
+    }                                                                                                     \
+    void name##_free(name##_t *array)                                                                     \
+    {                                                                                                     \
+        free((array)->items);                                                                             \
+        name##_init(array);                                                                               \
     }
 
 #endif // CLOX_ARRAY_H

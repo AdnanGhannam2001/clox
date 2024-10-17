bc4980f83c76612575904c0461d56f6634a8e728
AdnanGhannam2001 <adnanghannam21@outlook.com>
2024-10-17 17:19:39 +0300

    Fix `sizeof` issue

--- includes/array.h
+++ includes/array.h
@@ -17,27 +17,27 @@ typedef size_t array_size_t;
     void name##_write(name##_t *array, type value); \
     void name##_free(name##_t *array);
 
-#define ARRAY_IMPL(name, type)                                                                                           \
-    void name##_init(name##_t *array)                                                                                    \
-    {                                                                                                                    \
-        (array)->capacity = 0;                                                                                           \
-        (array)->count = 0;                                                                                              \
-        (array)->items = NULL;                                                                                           \
-    }                                                                                                                    \
-    void name##_write(name##_t *array, type value)                                                                       \
-    {                                                                                                                    \
-        if ((array)->count >= (array)->capacity)                                                                         \
-        {                                                                                                                \
-            (array)->capacity = GROW_CAPACITY((array)->capacity);                                                        \
-            (array)->items = (type *)memory_allocate((array)->items, ((array)->capacity * sizeof(array_size_t)), false); \
-        }                                                                                                                \
-        (array)->items[(array)->count] = (value);                                                                        \
-        (array)->count++;                                                                                                \
-    }                                                                                                                    \
-    void name##_free(name##_t *array)                                                                                    \
-    {                                                                                                                    \
-        memory_free((array)->items);                                                                                     \
-        name##_init(array);                                                                                              \
+#define ARRAY_IMPL(name, type)                                                                                   \
+    void name##_init(name##_t *array)                                                                            \
+    {                                                                                                            \
+        (array)->capacity = 0;                                                                                   \
+        (array)->count = 0;                                                                                      \
+        (array)->items = NULL;                                                                                   \
+    }                                                                                                            \
+    void name##_write(name##_t *array, type value)                                                               \
+    {                                                                                                            \
+        if ((array)->count >= (array)->capacity)                                                                 \
+        {                                                                                                        \
+            (array)->capacity = GROW_CAPACITY((array)->capacity);                                                \
+            (array)->items = (type *)memory_allocate((array)->items, ((array)->capacity * sizeof(type)), false); \
+        }                                                                                                        \
+        (array)->items[(array)->count] = (value);                                                                \
+        (array)->count++;                                                                                        \
+    }                                                                                                            \
+    void name##_free(name##_t *array)                                                                            \
+    {                                                                                                            \
+        memory_free((array)->items);                                                                             \
+        name##_init(array);                                                                                      \
     }
 
 #endif // CLOX_ARRAY_H
